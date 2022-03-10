8450b8779b2ffe69df2ad2a5ae299dd571f73974
André Rosa <25556799+andreffrosa@users.noreply.github.com>
2022-03-10 14:46:07 +0000

    created express app

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const port = 80;
+
+// Configure Express
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Create jobs path router
+var router = express.Router();
+
+// Start API Server
+app.listen(port, () => {
+    console.log(`Listening on port ${port} ...`);
+})
